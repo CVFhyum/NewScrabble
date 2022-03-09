b4a5bdc24e21032d4be311abd0bbbe9f24948504
cvf <cvfhyum@gmailcom>
2022-03-09 17:32:05 +0200

    pog

---
+++ test.py
@@ -0,0 +1,17 @@
+from termcolor import colored as cl
+from random import randint as ri, choice
+from time import sleep as s, perf_counter
+from string import ascii_lowercase as lc, ascii_uppercase as uc, printable
+
+class test:
+    pass
+
+lis = []
+lis.append(test())
+print(lis)
+
+
+
+
+
+
