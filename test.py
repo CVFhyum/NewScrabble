62a44ef97a510a1f3a5c024e5bc6d449078068c3
cvf <cvfhyum@gmailcom>
2022-06-01 21:54:51 +0300

    daqas

--- test.py
+++ test.py
@@ -3,17 +3,7 @@ from random import randint as ri, choice
 from time import sleep as s, perf_counter
 from string import ascii_lowercase as lc, ascii_uppercase as uc, printable
 
-def Move1Over(arr):
-    last = arr[-1]
-    temp = arr[0]
-    for i in range(len(arr)-1):
-        arr[i+1] = temp
-        temp = arr[i+2]
 
-    return arr
-
-l = ['h','m','n','p']
-print(Move1Over(l))
 
 
 
